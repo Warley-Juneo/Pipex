5d68981239f0de45e60ce949c09bb7df273e006e
Warley-Juneo <warleyjuneo111@gmail.com>
2021-10-28 20:42:50 -0300

    add: fucking bonus

--- bonus/pipex_bonus.c
+++ bonus/pipex_bonus.c
@@ -1,43 +1,6 @@
 #include "pipex_bonus.h"
 
-int	second_comands(int *tubo, char *file2, char *comands2)
-{
-	char	**cmd2;
-	int 	arq2;
-
-	cmd2 = ft_split(comands2, ' ');
-	cmd2[0] = ft_strjoin("/usr/bin/", cmd2[0]);
-	arq2 = open(file2, O_RDWR);
-	if (arq2 == -1)
-		return (write(2, "Arq2 Error!", 12));
-	else
-	{
-		dup2(tubo[0], STDIN_FILENO);
-		dup2(arq2, STDOUT_FILENO);
-		close(tubo[0]);
-		close(tubo[1]);
-		printf("Sua mae\n");
-		execve(cmd2[0], cmd2, NULL);
-	}
-	return (0);
-}
-
-int	exec_comands(int *tubo, char *comands)
-{
-	char	**cmd;
-
-	cmd = ft_split(comands, ' ');
-	cmd[0] = ft_strjoin("/usr/bin/", cmd[0]);
-
-	dup2(tubo[0], STDIN_FILENO);
-	dup2(tubo[1], STDOUT_FILENO);
-	close(tubo[0]);
-	close(tubo[1]);
-	execve(cmd[0], cmd, NULL);
-	return (0);
-}
-
-int	first_comands(int *tubo, char *file1, char *comands1)
+int	first_comand(t_tubo tubo, char *file1, char *comands1)
 {
 	char	**cmd1;
 	int 	arq1;
@@ -49,61 +12,104 @@ int	first_comands(int *tubo, char *file1, char *comands1)
 		return (write(2, "Arq1 Error!", 12));
 	else
 	{
-		dup2(tubo[1], STDOUT_FILENO);
+		dup2(tubo.tubo[1], STDOUT_FILENO);
 		dup2(arq1, STDIN_FILENO);
-		close(tubo[0]);
-		close(tubo[1]);
+		close(tubo.tubo[0]);
+		close(tubo.tubo[1]);
 		execve(cmd1[0], cmd1, NULL);
 	}
 	return (0);
 }
 
-int	pipex_bonus(int argc, char *argv[], char *envp[])
+int	last_comands(t_tubo tubo, char *file2, char *comands2)
 {
-	int	tubo[2];
-	int	pid1;
-	int	pid2;
-	int	pid3;
-	int	i;
+	char	**cmd2;
+	int 	arq2;
 
-	(void)envp;
-	if (pipe(tubo) == -1)
-		return (write(2, "Comunication Error!", 20));
-	pid1 = fork();
-	if (pid1 == -1)
-		return (write(2, "Fork Error (1)!", 16));
-	if (pid1 == 0)
+	cmd2 = ft_split(comands2, ' ');
+	cmd2[0] = ft_strjoin("/usr/bin/", cmd2[0]);
+	arq2 = open(file2, O_RDWR);
+	if (arq2 == -1)
+		return (write(2, "Arq2 Error!", 12));
+	else
 	{
-			first_comands(tubo, argv[1], argv[2]);
+		dup2(tubo.tubo[0], STDIN_FILENO);
+		dup2(arq2, STDOUT_FILENO);
+		close(tubo.tubo[0]);
+		close(tubo.tubo[1]);
+		execve(cmd2[0], cmd2, NULL);
 	}
-	printf("Sei lá\n");
-	i = 3;
-	while (i < argc - 2)
-	{
-		pid3 = fork();
-		if (pid3 == 0)
-			exec_comands(tubo, argv[i]);
-		i++;
-	}
-	printf("Sei láaaaa\n");
-	pid2 = fork();
-	if (pid2 == -1)
-		return (write(2, "Fork Error (2)!", 16));
-	if (pid2 == 0)
-	{
-		printf("%d\n", i);
-		second_comands(tubo, argv[i + 1], argv[i]);
-	}
-	close(tubo[1]);
-	close(tubo[0]);
-	waitpid(pid1, NULL, 0);
-	waitpid(pid2, NULL, 0);
 	return (0);
 }
 
+int	execve_comand(t_tubo fd, char *comands)
+{
+	char	**cmd;
+
+	cmd = ft_split(comands, ' ');
+	cmd[0] = ft_strjoin("/usr/bin/", cmd[0]);
+
+	dup2(fd.tubo[0], STDIN_FILENO);
+	dup2(fd.tubo[1], STDOUT_FILENO);
+	fd.temp = fd.tubo[1];
+	close(fd.tubo[1]);
+	close(fd.tubo[0]);
+	execve(cmd[0], cmd, NULL);
+	pipe(fd.tubo);
+	fd.tubo[0] = fd.temp;
+	return (0);
+}
+
+int	pipex(char *argv[], char *envp[], int argc)
+{
+	t_tubo	fd;
+	int pid1;
+	int pid2;
+	int pid3;
+	int i;
+
+	(void)envp;
+
+	if (pipe(fd.tubo) == -1)
+		perror("Comunication Error");
+	pid1 = fork();
+	if (pid1 == -1)
+		perror("Fork Error!");
+	if (pid1 == 0)
+		first_comand(fd, argv[1], argv[2]);
+	i = 3;
+	pid2 = fork();
+	if (pid2 == -1)
+		perror("Fork Error!");
+	if (pid2 == 0)
+	{
+		while(i < argc - 1)
+		{
+			execve_comand(fd, argv[i]);
+			i++;
+		}
+	}
+	pid3 = fork();
+	if (pid3 == -1)
+		perror("Fork Error!");
+	if (pid3 == 0)
+	{
+		last_comands(fd, argv[argc - 1], argv[i]);
+	}
+	close(fd.tubo[0]);
+	close(fd.tubo[1]);
+
+	waitpid(pid1, NULL, 0);
+	waitpid(pid2, NULL, 0);
+	waitpid(pid3, NULL, 0);
+	return (0);
+}
+
+
 int main(int argc, char *argv[], char *envp[])
 {
-	// printf("%d\n", access("/usr/bin/ls", X_OK));
-	pipex_bonus(argc, argv, envp);
+	// printf("%d\n", access("/usr/bin/grep", X_OK));
+	(void)argc;
+	pipex(argv, envp, argc);
 	return (0);
 }
--- bonus/pipex_bonus.h
+++ bonus/pipex_bonus.h
@@ -1,6 +1,13 @@
 #ifndef PIPEX_BONUS_H
 # define PIPEX_BONUS_H
 
+
+typedef	struct s_tubo
+{
+	int	tubo[2];
+	int	temp;
+}		t_tubo;
+
 #include "../libft/libft.h"
 #include <unistd.h>
 #include <stdlib.h>
